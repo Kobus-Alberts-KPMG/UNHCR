610f54d33a9fb32666a7ec6f9e9cbd4d414e9e9d
Andries Jacobus Alberts <andries.alberts@crimsonwing.com>
2023-11-08 13:22:18 +0100

    Added the IP logic back and commented the local IP

--- UNHCR.Geolocation/Infrastructure/IPHelper.cs
+++ UNHCR.Geolocation/Infrastructure/IPHelper.cs
@@ -20,12 +20,12 @@ namespace UNHCR.Geolocation.Infrastructure
             try
             {
                 var headerValues = new Dictionary<string, string>();
-#if DEBUG
-                var clientIP = "74.85.218.255";
-                return new OkObjectResult(clientIP);
-#endif
+//#if DEBUG
+//                var clientIP = "74.85.218.255";
+//                return new OkObjectResult(clientIP);
+//#endif
 
- /*               foreach (var header in req.Headers)
+                foreach (var header in req.Headers)
                 {
                     var headerValue = string.Join(":", header.Value);
                     headerValues.Add(header.Key, headerValue);
@@ -44,7 +44,7 @@ namespace UNHCR.Geolocation.Infrastructure
                     log.LogInformation("The 'X-Forwarded-For' header was not found.");
                     return new BadRequestObjectResult("Client IP was not found");
 
-                }*/
+                }
             }
             catch (Exception ex)
             {
