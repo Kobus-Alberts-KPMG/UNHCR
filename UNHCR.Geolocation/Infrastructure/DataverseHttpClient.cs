ce1ba0c34612e983aa00d697671c50685e3f82df
Andries Jacobus Alberts <andries.alberts@crimsonwing.com>
2023-11-09 13:50:31 +0100

    Added the unit tests

--- UNHCR.Geolocation/Infrastructure/DataverseHttpClient.cs
+++ UNHCR.Geolocation/Infrastructure/DataverseHttpClient.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Client;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +25,6 @@ namespace UNHCR.Geolocation.Infrastructure
             var clientId = keyVaultManager.GetSecretAsync("ClientId").GetAwaiter().GetResult();
             var clientSecret = keyVaultManager.GetSecretAsync("ClientSecret").GetAwaiter().GetResult();
             var scope = keyVaultManager.GetSecretAsync("Scope2").GetAwaiter().GetResult();
-
 
             TenantInfo _tenantInfo = new()
             {
@@ -53,6 +53,40 @@ namespace UNHCR.Geolocation.Infrastructure
             logger.LogInformation($"retrieved token for client {tenantInfo.ClientId} on tenant {tenantInfo.TenantId}");
             return await app.AcquireTokenForClient(new[] { tenantInfo.Scope }).ExecuteAsync();
         }
+
+        /// <summary>
+        /// Get managed identity token.
+        /// </summary>
+        /// <param name="resource">The AAD resource URI of the resource for which a token should be obtained. </param>
+        /// <param name="apiversion">The version of the token API to be used. "2017-09-01" is currently the only version supported.</param>
+        /// <param name="clientId">(Optional) The ID of the user-assigned identity to be used. If omitted, the system-assigned identity is used.</param>
+        /// <returns>A Bearer token ready to be used with AAD-authenticated REST API calls.</returns>
+        public  async Task<string> GetToken(string resource, string apiversion, string clientId = null)
+        {
+            var httpClient = httpClientFactory.CreateClient();
+            httpClient.DefaultRequestHeaders.Add("Secret", Environment.GetEnvironmentVariable("MSI_SECRET"));
+
+            string url;
+            if (clientId == null)
+            {
+                // Get system-assigned identity token
+                url = String.Format("{0}?resource={1}&api-version={2}", Environment.GetEnvironmentVariable("MSI_ENDPOINT"), resource, apiversion);
+            }
+            else
+            {
+                // Get user-assigned identity token
+                url = String.Format("{0}?resource={1}&api-version={2}&clientid={3}", Environment.GetEnvironmentVariable("MSI_ENDPOINT"), resource, apiversion, clientId);
+            }
+
+            HttpResponseMessage responseMessage = await httpClient.GetAsync(url);
+            string content = await responseMessage.Content.ReadAsStringAsync();
+
+            var result = JObject.Parse(content);
+            string accessToken = result["access_token"].ToString();
+
+            return accessToken;
+        }
+
 
 
         /// <summary>
---
+++ UNHCR.GeolocationTests/Infrastructure/CRMClientTests.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace UNHCR.GeolocationTests.Infrastructure
+{
+    [TestClass()]
+    public class CRMClientTests
+    {
+        [TestMethod]
+        public void TestCRMTestClient()
+        {
+            var managedIdentity = new DefaultAzureCredential(new DefaultAzureCredentialOptions()
+            {
+                ManagedIdentityClientId = "ba497ab9-d3ab-44d7-b52d-1b0fb07c8b80"//Environment.GetEnvironmentVariable("ManagedIdentityClientId")
+            });
+            var environment = "https://myunhcr-dev.crm4.dynamics.com";//Environment.GetEnvironmentVariable("EnvironmentUrl");
+
+            var client = new ServiceClient(new Uri(environment), tokenProviderFunction: async u =>
+               (
+                   await managedIdentity.GetTokenAsync(new TokenRequestContext(new[] { $"{environment}/.default" }))).Token
+                );
+            Assert.IsTrue(client.IsReady);
+            var result = client.RetrieveMultiple(new FetchExpression("<fetch mapping='logical'><entity name='progres_buenrollments'></entity></fetch>"));
+            Assert.IsNotNull(result);
+        }
+    }
+}
