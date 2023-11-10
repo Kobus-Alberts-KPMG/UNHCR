518ea9ac9f1f791b1e87c12223be9698b622a18a
Andries Jacobus Alberts <andries.alberts@crimsonwing.com>
2023-11-10 16:19:11 +0100

    fd

--- UNHCR.Geolocation/GeoFencing.cs
+++ UNHCR.Geolocation/GeoFencing.cs
@@ -18,35 +18,6 @@ using System.Reflection.Metadata;
 namespace UNHCR.Geolocation
 {
 
-    public class Rootobject
-    {
-        public List<Entity> entities { get; set; }
-        public bool moreRecords { get; set; }
-        public string pagingCookie { get; set; }
-        public string minActiveRowVersion { get; set; }
-        public int totalRecordCount { get; set; }
-        public bool totalRecordCountLimitExceeded { get; set; }
-        public string entityName { get; set; }
-    }
-
-    public class Entity
-    {
-        public string logicalName { get; set; }
-        public string id { get; set; }
-        public List<Attribute> attributes { get; set; }
-        public object entityState { get; set; }
-        public object[] formattedValues { get; set; }
-        public object[] relatedEntities { get; set; }
-        public string rowVersion { get; set; }
-        public object[] keyAttributes { get; set; }
-    }
-
-    public class Attribute
-    {
-        public string key { get; set; }
-        public string value { get; set; }
-    }
-
     public class GeoFencing
     {
         private readonly IKeyVaultManager keyVaultManager;
@@ -61,37 +32,15 @@ namespace UNHCR.Geolocation
             dataverseHttpClient = _dataverseHttpClient;
         }
 
-        [FunctionName("PingCRM")]
-        public async Task<IActionResult> PingCRM(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ILogger log)
-        {
-            var managedIdentity = new DefaultAzureCredential(new DefaultAzureCredentialOptions()
-            {
-                ManagedIdentityClientId = "ba497ab9-d3ab-44d7-b52d-1b0fb07c8b80"
-            });
-
-            var environment = "https://myunhcr-dev.crm4.dynamics.com";
-
-            var client = new ServiceClient(new Uri(environment), tokenProviderFunction: async u => (
-                await managedIdentity.GetTokenAsync(new TokenRequestContext(new[] { $"{environment}/.default" }))).Token
-            );
-            var result = client.RetrieveMultiple(new FetchExpression("<fetch version=\"1.0\" output-format=\"xml-platform\" mapping=\"logical\" distinct=\"false\">\r\n<entity name=\"progres_countryterritory\">\r\n<attribute name=\"progres_countryterritoryid\" />\r\n<attribute name=\"progres_name\" />\r\n<attribute name=\"progres_isocode3\" />\r\n<attribute name=\"progres_progresguid\" />\r\n<attribute name=\"progres_isocode2\" />\r\n<order attribute=\"progres_name\" descending=\"false\" />\r\n<filter type=\"and\">\r\n<condition attribute=\"progres_isocode2\" operator=\"eq\" value=\"MT\" />\r\n</filter>\r\n</entity>\r\n</fetch>"));
-
-            return new OkObjectResult(result);
-        }
-
-
         [FunctionName("GeoFencing")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ILogger log)
         {
-
-
             subkey = await keyVaultManager.GetSecretAsync("AtlasSubscriptionKey");
 
             //// Retrieve the client IP
             var clientIPResult = IPHelper.GetClientIP(req, log);
-            if (!(clientIPResult is OkObjectResult okResult))
+            if (clientIPResult is not OkObjectResult okResult)
             {
                 return clientIPResult;
             }
@@ -125,7 +74,11 @@ namespace UNHCR.Geolocation
                     await managedIdentity.GetTokenAsync(new TokenRequestContext(new[] { $"{environment}" }))).Token
                 );
 
-                var territories = client.RetrieveMultiple(new FetchExpression($"<fetch version=\"1.0\" output-format=\"xml-platform\" mapping=\"logical\" distinct=\"false\">\r\n<entity name=\"progres_countryterritory\">\r\n<attribute name=\"progres_countryterritoryid\" />\r\n<attribute name=\"progres_name\" />\r\n<attribute name=\"progres_isocode3\" />\r\n<attribute name=\"progres_progresguid\" />\r\n<attribute name=\"progres_isocode2\" />\r\n<order attribute=\"progres_name\" descending=\"false\" />\r\n<filter type=\"and\">\r\n<condition attribute=\"progres_isocode2\" operator=\"eq\" value=\"{isocode}\" />\r\n</filter>\r\n</entity>\r\n</fetch>"));
+                var territories = client.RetrieveMultiple(new FetchExpression($"<fetch version=\"1.0\" output-format=\"xml-platform\" mapping=\"logical\" " +
+                    $"distinct=\"false\">\r\n<entity name=\"progres_countryterritory\">\r\n<attribute name=\"progres_countryterritoryid\" />\r\n<attribute " +
+                    $"name=\"progres_name\" />\r\n<attribute name=\"progres_isocode3\" />\r\n<attribute name=\"progres_progresguid\" />\r\n<attribute " +
+                    $"name=\"progres_isocode2\" />\r\n<order attribute=\"progres_name\" descending=\"false\" />\r\n<filter type=\"and\">\r\n<condition " +
+                    $"attribute=\"progres_isocode2\" operator=\"eq\" value=\"{isocode}\" />\r\n</filter>\r\n</entity>\r\n</fetch>"));
 
                 bool isMatchFound = false;
 
